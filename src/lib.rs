edf05d7fc021ee8ce5a58e0601047112132e7257
maedana <maeda.na@gmail.com>
2021-05-03 22:10:21 +0900

    メインからロジックをlibsに移動

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
 extern crate open;
 
+use seahorse::{Command, Context};
+use std::convert::TryInto;
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::screen::*;
+
 #[derive(Debug)]
 pub struct Report {
     title: String,
@@ -31,4 +39,70 @@ impl Report {
     pub fn open(&self) {
         open::that(&self.url);
     }
+}
+
+pub fn show_reports_command() -> Command {
+    Command::new("reports")
+        .description("Show unchecked reports.")
+        .alias("r")
+        .usage("fjord-cli reports(r)")
+        .action(show_reports_action)
+}
+
+fn write_alt_screen_msg<W: Write>(screen: &mut W, reports: &Vec<Report>) {
+    write!(screen, "{}", termion::clear::All).unwrap();
+    for (i, report) in reports.iter().enumerate() {
+        write!(
+            screen,
+            "{}{}",
+            termion::cursor::Goto(1, (i + 1).try_into().unwrap()),
+            report.screen_label()
+        )
+        .unwrap();
+    }
+}
+
+fn show_reports_action(_c: &Context) {
+    let stdin = stdin();
+    let mut screen = AlternateScreen::from(stdout().into_raw_mode().unwrap());
+    write!(screen, "{}", termion::cursor::Hide).unwrap();
+    let reports = Report::fetch();
+    write_alt_screen_msg(&mut screen, &reports);
+
+    // カーソルを最初の位置へセット
+    let cursor_x = 1;
+    let mut cursor_y = 1;
+    write!(
+        screen,
+        "{}{}",
+        termion::cursor::Goto(cursor_x, cursor_y),
+        termion::cursor::Show
+    )
+    .unwrap();
+
+    screen.flush().unwrap();
+
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Char('q') => break,
+            Key::Ctrl('c') => break,
+            Key::Char('j') => {
+                cursor_y += 1;
+            }
+            Key::Char('k') => {
+                if cursor_y > 1 {
+                    cursor_y -= 1;
+                }
+            }
+            Key::Char('o') => {
+                let current_index: usize = From::from(cursor_y - 1);
+                let report = &reports[current_index];
+                report.open();
+            }
+            _ => {}
+        }
+        write!(screen, "{}", termion::cursor::Goto(cursor_x, cursor_y)).unwrap();
+        screen.flush().unwrap();
+    }
+    write!(screen, "{}", termion::cursor::Show).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
 extern crate fjord_cli;
 extern crate termion;
 
-use fjord_cli::Report;
-use seahorse::{App, Command, Context};
-use std::convert::TryInto;
+use seahorse::App;
 use std::env;
-use std::io::{stdin, stdout, Write};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-use termion::screen::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,73 +11,7 @@ fn main() {
         .author(env!("CARGO_PKG_AUTHORS"))
         .version(env!("CARGO_PKG_VERSION"))
         .usage("fjord-cli [name]")
-        .command(show_reports_command());
+        .command(fjord_cli::show_reports_command());
 
     app.run(args);
-}
-
-fn write_alt_screen_msg<W: Write>(screen: &mut W, reports: &Vec<Report>) {
-    write!(screen, "{}", termion::clear::All).unwrap();
-    for (i, report) in reports.iter().enumerate() {
-        write!(
-            screen,
-            "{}{}",
-            termion::cursor::Goto(1, (i + 1).try_into().unwrap()),
-            report.screen_label()
-        )
-        .unwrap();
-    }
-}
-
-fn show_reports_action(_c: &Context) {
-    let stdin = stdin();
-    let mut screen = AlternateScreen::from(stdout().into_raw_mode().unwrap());
-    write!(screen, "{}", termion::cursor::Hide).unwrap();
-    let reports = Report::fetch();
-    write_alt_screen_msg(&mut screen, &reports);
-
-    // カーソルを最初の位置へセット
-    let cursor_x = 1;
-    let mut cursor_y = 1;
-    write!(
-        screen,
-        "{}{}",
-        termion::cursor::Goto(cursor_x, cursor_y),
-        termion::cursor::Show
-    )
-    .unwrap();
-
-    screen.flush().unwrap();
-
-    for c in stdin.keys() {
-        match c.unwrap() {
-            Key::Char('q') => break,
-            Key::Ctrl('c') => break,
-            Key::Char('j') => {
-                cursor_y += 1;
-            }
-            Key::Char('k') => {
-                if cursor_y > 1 {
-                    cursor_y -= 1;
-                }
-            }
-            Key::Char('o') => {
-                let current_index: usize = From::from(cursor_y - 1);
-                let report = &reports[current_index];
-                report.open();
-            }
-            _ => {}
-        }
-        write!(screen, "{}", termion::cursor::Goto(cursor_x, cursor_y)).unwrap();
-        screen.flush().unwrap();
-    }
-    write!(screen, "{}", termion::cursor::Show).unwrap();
-}
-
-fn show_reports_command() -> Command {
-    Command::new("reports")
-        .description("Show unchecked reports.")
-        .alias("r")
-        .usage("fjord-cli reports(r)")
-        .action(show_reports_action)
 }
