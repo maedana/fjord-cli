781f98e0353ad3b6ac9160611a7c0b5e4c01c96d
maedana <maeda.na@gmail.com>
2021-05-03 16:02:14 +0900

    未チェック日報の表示を呼び出すところまで

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use seahorse::{App, Command, Context};
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let app = App::new(env!("CARGO_PKG_NAME"))
+        .description(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .usage("fjord-cli [name]")
+        .command(show_reports_command());
+
+    app.run(args);
+}
+
+fn show_reports_action(_c: &Context) {
+    println!("show reports!!!");
+}
+
+fn show_reports_command() -> Command {
+    Command::new("reports")
+        .description("show unchecked reports")
+        .alias("r")
+        .usage("fjord-cli reports(r)")
+        .action(show_reports_action)
 }
