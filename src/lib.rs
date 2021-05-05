cb5045788f7bff25ef0b58cf4683bc77b81992ed
maedana <maeda.na@gmail.com>
2021-05-05 15:57:27 +0900

    タブが出るところまで

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,10 @@ use std::{error::Error, io};
 use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
 use tui::{
     backend::TermionBackend,
-    layout::{Constraint, Layout},
+    layout::{Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
-    widgets::{Block, Borders, Cell, Row, Table},
+    text::{Span, Spans},
+    widgets::{Block, Borders, Cell, Row, Table, Tabs},
     Terminal,
 };
 use util::event::{Event, Events};
@@ -75,9 +76,36 @@ impl Report {
     }
 }
 
+pub struct TabsState<'a> {
+    pub titles: Vec<&'a str>,
+    pub index: usize,
+}
+
+impl<'a> TabsState<'a> {
+    pub fn new(titles: Vec<&'a str>) -> TabsState {
+        TabsState { titles, index: 0 }
+    }
+    pub fn next(&mut self) {
+        self.index = (self.index + 1) % self.titles.len();
+    }
+
+    pub fn previous(&mut self) {
+        if self.index > 0 {
+            self.index -= 1;
+        } else {
+            self.index = self.titles.len() - 1;
+        }
+    }
+}
+
+struct App<'a> {
+    tabs: TabsState<'a>,
+}
+
 pub fn reports_action(_c: &Context) {
     render_reports().unwrap()
 }
+
 fn render_reports() -> Result<(), Box<dyn Error>> {
     // Terminal initialization
     let stdout = io::stdout().into_raw_mode()?;
@@ -87,6 +115,15 @@ fn render_reports() -> Result<(), Box<dyn Error>> {
     let mut terminal = Terminal::new(backend)?;
 
     let events = Events::new();
+
+    let mut app = App {
+        tabs: TabsState::new(vec![
+            "Report(0)",
+            "Unchecked Product(0)",
+            "Unassigned Product(0)",
+            "Assigned Product(0)",
+        ]),
+    };
 
     let reports = Report::fetch();
     let report_items: Vec<Vec<String>> = reports
@@ -104,10 +141,32 @@ fn render_reports() -> Result<(), Box<dyn Error>> {
     // Input
     loop {
         terminal.draw(|f| {
-            let rects = Layout::default()
-                .constraints([Constraint::Percentage(100)].as_ref())
+            //            let rects = Layout::default()
+            //                .constraints([Constraint::Percentage(100)].as_ref())
+            //                .margin(5)
+            //                .split(f.size());
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
                 .margin(5)
+                .constraints([Constraint::Length(3), Constraint::Min(0)].as_ref())
                 .split(f.size());
+
+            let titles = app
+                .tabs
+                .titles
+                .iter()
+                .map(|t| Spans::from(vec![Span::styled(*t, Style::default().fg(Color::White))]))
+                .collect();
+            let tabs = Tabs::new(titles)
+                .block(Block::default().borders(Borders::ALL).title("Tabs"))
+                .select(app.tabs.index)
+                .style(Style::default().fg(Color::White))
+                .highlight_style(
+                    Style::default()
+                        .add_modifier(Modifier::BOLD)
+                        .bg(Color::Black),
+                );
+            f.render_widget(tabs, chunks[0]);
 
             let selected_style = Style::default().add_modifier(Modifier::REVERSED);
             let normal_style = Style::default().bg(Color::White);
@@ -142,7 +201,7 @@ fn render_reports() -> Result<(), Box<dyn Error>> {
                     Constraint::Length(30),
                     Constraint::Max(10),
                 ]);
-            f.render_stateful_widget(t, rects[0], &mut table.state);
+            f.render_stateful_widget(t, chunks[1], &mut table.state);
         })?;
 
         if let Event::Input(key) = events.next()? {
