4efa0bfe7885f803c658a3eab841f284bca811e6
maedana <maeda.na@gmail.com>
2021-05-05 21:06:32 +0900

    Reportを単独ファイルに切り出し

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 extern crate open;
 extern crate serde_json;
 
+mod models;
 mod util;
+
+use models::report::Report;
 use seahorse::Context;
 use std::env;
 use std::thread;
@@ -19,65 +22,8 @@ use tui::{
 use util::event::{Event, Events};
 use util::stateful_table::StatefulTable;
 
-// MEMO: Reportは個別ファイルにしたい
-#[derive(Debug)]
-pub struct Report {
-    title: String,
-    url: String,
-    reported_on: String,
-    login_name: String,
-}
-
-impl Report {
-    pub fn fetch() -> Vec<Report> {
-        let mut page = 1;
-        let mut reports = vec![];
-        loop {
-            let url = format!(
-                "https://bootcamp.fjord.jp/api/reports/unchecked.json?page={}",
-                page
-            );
-            let resp = ureq::get(&url)
-                .set("Authorization", &env::var("FJORD_JWT_TOKEN").unwrap())
-                .call()
-                .unwrap();
-            let json: serde_json::Value = resp.into_json().unwrap();
-            let report_array = json["reports"].as_array().unwrap();
-            if report_array.is_empty() {
-                break;
-            }
-            for r in json["reports"].as_array().unwrap().iter() {
-                reports.push(Report {
-                    title: r["title"].as_str().unwrap().to_string(),
-                    url: r["url"].as_str().unwrap().to_string(),
-                    reported_on: r["reportedOn"].as_str().unwrap().to_string(),
-                    login_name: r["user"]["login_name"].as_str().unwrap().to_string(),
-                })
-            }
-            page += 1;
-            thread::sleep(Duration::from_millis(500));
-        }
-        reports
-    }
-
-    pub fn title(&self) -> &str {
-        &self.title
-    }
-
-    pub fn reported_on(&self) -> &str {
-        &self.reported_on
-    }
-
-    pub fn login_name(&self) -> &str {
-        &self.login_name
-    }
-
-    pub fn open(&self) {
-        open::that(&self.url).unwrap();
-    }
-}
-
 // MEMO: Produtは個別ファイルにしたい
+#[derive(Debug)]
 pub struct Product {
     title: String,
     url: String,
---
+++ src/models/report.rs
@@ -0,0 +1,60 @@
+use std::env;
+use std::thread;
+use std::time::Duration;
+
+#[derive(Debug)]
+pub struct Report {
+    title: String,
+    url: String,
+    reported_on: String,
+    login_name: String,
+}
+
+impl Report {
+    pub fn fetch() -> Vec<Report> {
+        let mut page = 1;
+        let mut reports = vec![];
+        loop {
+            let url = format!(
+                "https://bootcamp.fjord.jp/api/reports/unchecked.json?page={}",
+                page
+            );
+            let resp = ureq::get(&url)
+                .set("Authorization", &env::var("FJORD_JWT_TOKEN").unwrap())
+                .call()
+                .unwrap();
+            let json: serde_json::Value = resp.into_json().unwrap();
+            let report_array = json["reports"].as_array().unwrap();
+            if report_array.is_empty() {
+                break;
+            }
+            for r in json["reports"].as_array().unwrap().iter() {
+                reports.push(Report {
+                    title: r["title"].as_str().unwrap().to_string(),
+                    url: r["url"].as_str().unwrap().to_string(),
+                    reported_on: r["reportedOn"].as_str().unwrap().to_string(),
+                    login_name: r["user"]["login_name"].as_str().unwrap().to_string(),
+                })
+            }
+            page += 1;
+            thread::sleep(Duration::from_millis(500));
+        }
+        reports
+    }
+
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+
+    pub fn reported_on(&self) -> &str {
+        &self.reported_on
+    }
+
+    pub fn login_name(&self) -> &str {
+        &self.login_name
+    }
+
+    pub fn open(&self) {
+        open::that(&self.url).unwrap();
+    }
+}
